4579a31af8ff5d201aa7e8baaf00b8285e8f71bc
Jessica <jessica121353@gmail.com>
2015-12-03 18:57:38 +0100

    hideme

---
+++ hideMe.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Mumriken on 2015-11-19.
+ */
+$(function() {
+
+    $("#searchField").hide();
+
+    $("#searchButton").click(function() {
+        $("#searchField").toggle("slow");
+    });
+
+});
