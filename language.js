2650cee113f028d0d9c18a5d68d35a35fe705254
Jessica <jessica121353@gmail.com>
2015-12-07 18:34:49 +0100

    user page can change in 3 languages

---
+++ language.js
@@ -0,0 +1,32 @@
+/**
+ * Created by jessica on 12/4/15.
+ */
+
+//Taiwan
+Lang_tw = new Array();
+Lang_tw[0] = "想喝什麼啤酒？！";
+Lang_tw[1] = "搜尋";
+Lang_tw[2] = "將想要的啤酒托移到購物車";
+Lang_tw[3] = "購物車";
+Lang_tw[4] = "總價";
+Lang_tw[5] = "現在結帳";
+
+//English
+Lang_en = new Array();
+Lang_en[0] = "Place your order here";
+Lang_en[1] = "Search";
+Lang_en[2] = "Drag the beer you want to the order list";
+Lang_en[3] = "Order List";
+Lang_en[4] = "Total Price";
+Lang_en[5] = "Pay now";
+
+//Swedish
+Lang_se = new Array();
+Lang_se[0] = "Gör din beställning här";
+Lang_se[1] = "Söka";
+Lang_se[2] = "Dra öl du vill beställningslistan";
+Lang_se[3] = "beställningslista";
+Lang_se[4] = "Totalbelopp";
+Lang_se[5] = "Betala nu";
+
+
